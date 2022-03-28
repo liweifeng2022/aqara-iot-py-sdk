5f78812a6a4ff63821900156f8725f5db4cc5c5b
eric <liuyang.chen@aqara.com>
2022-03-28 20:44:39 +0800

    修改参数名字

--- aqara_iot/openapi.py
+++ aqara_iot/openapi.py
@@ -193,7 +193,7 @@ class AqaraOpenAPI:
             "response_type":"code",
             "redirect_uri":"https://www.baidu.com/",
             "account":username,
-            "passwd_md5":passwd_md5,
+            "passwordMD5":passwd_md5,
             "state":0
         }
         resp = requests.post(self.endpoint+PATH_AUTH,data=req_data,headers=headers)
